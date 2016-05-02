2f2b0eaa8f7ba35f5344dd59abbaa1834cca76d3
benjaml <b.lefevre@rubika-edu.com>
2016-05-02 17:22:53 +0200

    BEN AI v2

---
+++ Assets/bulletScriptBen.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bulletScriptBen : MonoBehaviour {
+
+    float Speed = 8;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += transform.forward * Speed * Time.deltaTime;
+	}
+    void OnCollisionEnter(Collision col)
+    {
+
+    }
+}
