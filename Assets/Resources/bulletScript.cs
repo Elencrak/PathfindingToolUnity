c91c57f1fbbf1f950abfe4e46b4dbdb3578cb3c3
unknown <v.birembaut@J001-0133-07.etudiant.rubika-edu.com.local>
2016-05-02 17:22:19 +0200

    loul

---
+++ Assets/Resources/bulletScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bulletScript : MonoBehaviour {
+
+    float speed = 40f;
+	// Use this for initialization
+	void Start () {
+        Destroy(gameObject, 12);
+
+        InvokeRepeating("Move", 0, .01f);
+	}
+	
+    void Move()
+    {
+        transform.position = transform.position + transform.forward * Time.deltaTime * speed;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        Destroy(gameObject);
+    }
+}
