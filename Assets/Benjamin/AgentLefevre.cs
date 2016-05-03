595fcaba5dde84460890f5ac5407c8321439c374
benjaml <b.lefevre@rubika-edu.com>
2016-05-03 11:13:32 +0200

    benModifs

--- Assets/Benjamin/AgentLefevre.cs
+++ Assets/Benjamin/AgentLefevre.cs
@@ -39,11 +39,13 @@ public class AgentLefevre : MonoBehaviour
             
         }
         spawn = transform.position;
-        coverPoints = new Vector3[4];
+        coverPoints = new Vector3[6];
         coverPoints[0] = transform.GetChild(0).position;
         coverPoints[1] = transform.GetChild(1).position;
         coverPoints[2] = transform.GetChild(2).position;
         coverPoints[3] = transform.GetChild(3).position;
+        coverPoints[4] = transform.GetChild(4).position;
+        coverPoints[5] = transform.GetChild(5).position;
         currentCover = 0;
 
         /*
@@ -142,11 +144,13 @@ public class AgentLefevre : MonoBehaviour
         }
         else
         {
+            Vector3 startPos = target.transform.position;
             agent.Resume();
             agent.SetDestination(coverPoints[coverPointIndex + 1]);
             yield return new WaitForSeconds(0.5f);
+            Vector3 direction = target.transform.position-startPos;
             agent.Stop();
-            Vector3 relativePos = target.transform.position - coverPoints[coverPointIndex + 1];
+            Vector3 relativePos = (target.transform.position+ direction) - coverPoints[coverPointIndex + 1];
             Quaternion rotation = Quaternion.LookRotation(relativePos);
 
             GameObject instance = Instantiate(bullet, transform.position+ relativePos.normalized*2.0f, rotation) as GameObject;
