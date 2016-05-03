ad2e7d4b4504f24230e145a7f4465b0617998d57
julien calvo <j.calvo@rubika-edu.com>
2016-05-03 10:35:32 +0200

    IA shoot

---
+++ Assets/Resources/bulletScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bulletScript : MonoBehaviour {
+
+    float speed = 40f;
+    public string launcherName;
+	// Use this for initialization
+	void Start () {
+        Destroy(gameObject, 12);
+
+        InvokeRepeating("Move", 0, .01f);
+	}
+	
+    void Move()
+    {
+        transform.position = transform.position + transform.forward * Time.deltaTime * speed;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        Destroy(gameObject);
+    }
+}
