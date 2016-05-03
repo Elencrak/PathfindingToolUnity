652f579f07597b27df09bbe15bb533f213491271
MrChuckchucky <b.devienne@rubika-edu.com>
2016-05-03 10:02:52 +0200

    AI evolution

---
+++ Assets/Poulpe/Scripts/Poulpe2.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Poulpe2 : MonoBehaviour
+{
+    private Vector3 begin;
+    private GameObject leader;
+	// Use this for initialization
+	void Start ()
+    {
+        begin = transform.position;
+        leader = transform.parent.GetChild(0).gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Bullet")
+        {
+            GetComponent<NavMeshAgent>().Warp(begin);
+        }
+    }
+}
