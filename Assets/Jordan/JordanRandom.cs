ef2446215e075f3a71ab90cb7650faf9fdbdb0a7
Windywyll <appleteensee@yahoo.fr>
2016-05-03 11:36:25 +0200

    enlever le cheat kamikaze et ajout du random follower

--- Assets/Jordan/JordanRandom.cs
+++ Assets/Jordan/JordanRandom.cs
@@ -2,18 +2,30 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class JordanKamikazeAgent : MonoBehaviour {
+public class JordanRandom : MonoBehaviour {
+
 
     private GameObject bullet;
     private List<GameObject> enemies;
     private Vector3 initPos;
+    private Transform target;
     public List<Transform> points;
     private int count;
     private bool touchedEveryone = false;
     private NavMeshAgent nav;
     private float startFireCoolDown;
     private float fireCoolDown = 1.0f;
-    private Transform target;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "Bullet")
+        {
+            randomTarget();
+        }
+
+        if (col.tag == "Target")
+            target = col.transform;
+    }
 
     void OnCollisionEnter(Collision col)
     {
@@ -21,18 +33,16 @@ public class JordanKamikazeAgent : MonoBehaviour {
         {
             if (col.gameObject.GetComponent<bulletScript>().launcherName != "Pelolance")
             {
-                target = col.gameObject.transform;
-
-                if (fireCoolDown + startFireCoolDown < Time.time && target != null)
-                    fire();
-
                 nav.Warp(initPos);
+                randomTarget();
             }
         }
     }
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
+
         bullet = Resources.Load("Bullet") as GameObject;
 
         initPos = this.transform.position;
@@ -42,20 +52,38 @@ public class JordanKamikazeAgent : MonoBehaviour {
         enemies = new List<GameObject>(temp);
 
         enemies.Remove(gameObject);
+        GameObject tempGO = GameObject.Find("VieuxPlanqueAgent");
+        enemies.Remove(tempGO);
+        tempGO = GameObject.Find("NouveauPlanqueAgent");
+        enemies.Remove(tempGO);
 
         count = 0;
 
         nav = gameObject.GetComponent<NavMeshAgent>();
 
         target = null;
+
+        randomTarget();
     }
-	
-	// Update is called once per frame
-	void Update () {
-        if (transform.position.x != points[0].position.x && transform.position.z != points[0].position.z)
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Physics.Raycast(transform.position, target.position - transform.position, out hit, 10000.0f);
+
+        if (fireCoolDown + startFireCoolDown < Time.time && hit.collider.tag == "Target")
+            fire();
+
+        if (transform.position != target.position)
         {
-            nav.SetDestination(points[0].position);
+            nav.SetDestination(target.position);
         }
+    }
+
+    void randomTarget()
+    {
+        target = enemies[Random.Range(0, enemies.Count)].transform;
     }
 
     void fire()
@@ -63,7 +91,7 @@ public class JordanKamikazeAgent : MonoBehaviour {
         startFireCoolDown = Time.time;
         Object temp = Instantiate(bullet);
         ((GameObject)temp).transform.position = this.transform.position + this.transform.forward;
-        ((GameObject)temp).transform.LookAt(target.position - target.forward);
+        ((GameObject)temp).transform.LookAt(target.position + target.forward * 3);
         ((GameObject)temp).GetComponent<bulletScript>().launcherName = "Pelolance";
     }
 }
