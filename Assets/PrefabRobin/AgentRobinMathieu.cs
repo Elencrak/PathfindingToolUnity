2e276ddafab6d0efc1478f0b5f606280465be52e
unknown <r.mathieu@J001-0140-15.etudiant.rubika-edu.com.local>
2016-05-03 10:28:13 +0200

    Mod squad shoot

--- Assets/PrefabRobin/AgentRobinMathieu.cs
+++ Assets/PrefabRobin/AgentRobinMathieu.cs
@@ -2,89 +2,76 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class AgentRobinMathieu : MonoBehaviour {
+public class AgentRobinMathieu : MonoBehaviour
+{
 
     [Header("IA")]
 
     public Vector3 startPoint;
     public GameObject nearestTarget;
-    BoxCollider nearTargetCollider;
+    public BoxCollider nearTargetCollider;
     public List<GameObject> targets;
     public List<GameObject> hitTargets;
     NavMeshAgent agent;
     bool hasWin = false;
 
-    [Header("GUN")]
-
-    public float RoF = 1.0f;
-    public List<GameObject> bullets;
-    public GameObject prefabBullet;
-    GameObject bulletList;
-
     [Header("Values")]
 
     float timeFreezeEnemy = 1.0f;
     public bool isShooting = true;
+    public static string playerID = "Squad Robin";
+    TeamNumber parentNumber;
 
-    // Use this for initialization
-    void Start () {
+    protected virtual void Start()
+    {
 
-        bullets = new List<GameObject>();
-        if(prefabBullet == null)
-        {
-            prefabBullet = Resources.Load<GameObject>("Bullet");
-        }
-        bulletList = transform.parent.GetChild(1).gameObject;
+        parentNumber = transform.parent.parent.GetComponent<TeamNumber>();
+        parentNumber.teamName = playerID;
         startPoint = transform.position;
         targets = new List<GameObject>(GameObject.FindGameObjectsWithTag("Target"));
         hitTargets = new List<GameObject>();
         agent = GetComponent<NavMeshAgent>();
-        InvokeRepeating("UpdateTarget", 0.0f, 0.5f);
-        InvokeRepeating("UpdateRoad", 0.0f, 0.8f);
+
+        for (int i = targets.Count - 1; i >= 0; --i)
+        {
+            if (targets[i].name == "Robin")
+            {
+                targets.Remove(targets[i]);
+            }
+        }
         InvokeRepeating("Gagne", 0.0f, 1.5f);
-        StartCoroutine(Shoot());
     }
-	
-	// Update is called once per frame
-	void Update ()
-    {
 
-	}
-
-	void Gagne ()
+    void Gagne()
     {
         bool isWin = true;
         foreach (GameObject target in targets)
         {
             isWin = isWin && hitTargets.Contains(target);
         }
-        if(isWin && !hasWin)
+        if (isWin && !hasWin)
         {
             hasWin = true;
             Debug.Log("J'ai gagné : Robin MATHIEU");
         }
-	}
+    }
 
-    void UpdateTarget()
+    protected virtual void UpdateTarget()
     {
 
         GameObject target = null;
 
-        for (int i = targets.Count - 1; i >= 0 ; --i)
+        for (int i = targets.Count - 1; i >= 0; --i)
         {
-            if (targets[i] == gameObject)
-            {
-                targets.Remove(targets[i]);
-            }
             if (target == null || (target && Vector3.Distance(target.transform.position, transform.position) > Vector3.Distance(targets[i].transform.position, transform.position)))
             {
-                if(!hitTargets.Contains(targets[i]))
+                if (!hitTargets.Contains(targets[i]))
                 {
                     target = targets[i];
                 }
             }
         }
-        if(target)
+        if (target)
         {
             nearestTarget = target;
             nearTargetCollider = target.GetComponent<BoxCollider>();
@@ -96,47 +83,21 @@ public class AgentRobinMathieu : MonoBehaviour {
         }
     }
 
-    /*void UpdateRoad()
-    {
-        if(nearestTarget)
-        {
-            agent.SetDestination(nearTargetCollider.transform.position + nearTargetCollider.center);
-        }
-    }*/
-
-    void UpdateRoad()
+    protected virtual void UpdateRoad()
     {
         agent.SetDestination(targets[Random.Range(0, targets.Count - 1)].transform.position);
     }
 
     void OnCollisionEnter(Collision coll)
     {
-        if(coll.gameObject.CompareTag("Target"))
+        if (coll.gameObject.CompareTag("Target") && coll.gameObject.name != "Robin")
         {
             StartCoroutine(FreezeEnemy(coll.gameObject));
             hitTargets.Add(coll.gameObject);
         }
-        if(coll.gameObject.CompareTag("Bullet") && !bullets.Contains(coll.gameObject))
+        if (coll.gameObject.CompareTag("Bullet") && !coll.gameObject.GetComponent<bulletScript>().launcherName.Equals(playerID))
         {
             Restart();
-        }
-    }
-
-    IEnumerator Shoot()
-    {
-        while(isShooting)
-        {
-            if(nearTargetCollider)
-            {
-                Vector3 direction = (nearTargetCollider.transform.position + nearTargetCollider.center) - transform.position;
-
-                GameObject go = Instantiate(prefabBullet, transform.position + direction.normalized * 2.0f, Quaternion.LookRotation(direction.normalized)) as GameObject;
-
-                go.transform.parent = bulletList.transform;
-
-                bullets.Add(go);
-            }
-            yield return new WaitForSeconds(RoF);
         }
     }
 
---
+++ Assets/PrefabRobin/AgentTripleRobin.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AgentTripleRobin : MonoBehaviour
+{
+    GameObject Target;
+    BoxCollider TargetCollider;
+
+    [Header("GUN")]
+
+    public float RoF = 1.0f;
+    public List<GameObject> bullets;
+    public GameObject prefabBullet;
+
+
+    AgentSimpleRobin tankUnit;
+
+    void Start()
+    {
+        bullets = new List<GameObject>();
+        if (prefabBullet == null)
+        {
+            prefabBullet = Resources.Load<GameObject>("Bullet");
+        }
+        tankUnit = transform.parent.parent.GetComponent<AgentSimpleRobin>();
+        StartCoroutine(Shoot());
+
+        InvokeRepeating("UpdateTarget", 0.0f, 0.5f);
+        InvokeRepeating("UpdateRoad", 0.0f, 0.8f);
+    }
+
+    void UpdateTarget()
+    {
+
+        GameObject target = tankUnit.targets[Random.Range(0, tankUnit.targets.Count - 1)];
+
+        if (target)
+        {
+            Target = target;
+            TargetCollider = target.GetComponent<BoxCollider>();
+        }
+        else
+        {
+            Target = null;
+            TargetCollider = null;
+        }
+    }
+
+    IEnumerator Shoot()
+    {
+        while (tankUnit.isShooting)
+        {
+            if (TargetCollider)
+            {
+                Vector3 direction = (TargetCollider.transform.position + TargetCollider.center) - transform.position;
+
+                GameObject go = Instantiate(prefabBullet, transform.position + direction.normalized * 2.0f, Quaternion.LookRotation(direction.normalized)) as GameObject;
+                
+                go.GetComponent<bulletScript>().launcherName = AgentRobinMathieu.playerID;
+
+                bullets.Add(go);
+            }
+            yield return new WaitForSeconds(RoF);
+        }
+    }
+}
