7f17d3e336fa2390fce8ff888ed3aec4a636d509
unknown <pi.martinez@J001-0135-05.etudiant.rubika-edu.com.local>
2016-05-18 14:10:04 +0200

    test state machine pierre

--- Assets/Pierre/PierreStateMachine.cs
+++ Assets/Pierre/PierreStateMachine.cs
@@ -1,15 +1,71 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class PierreStateMachine : MonoBehaviour {
+public class PierreStateMachine : PierreState
+{
+    List<PierreState> states;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    public PierreState currentState;
+
+    Pathfinding myPathfinding;
+
+    Vector3 currentTarget;
+
+    public Transform target;
+
+    List<Vector3> road;
+
+    // Use this for initialization
+    void Start ()
+    {
+        myPathfinding = new Pathfinding();
+        myPathfinding.Load("PierrePathFinding2");
+        myPathfinding.setNeighbors();
+
+
+
+        road = PathfindingManager.GetInstance().GetRoad(transform.position, target.position, myPathfinding);
+
+        road = PathfindingManager.GetInstance().SmoothRoad(road);
+    }
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+
+        Move();
+
+        Fire();
+
 	}
+
+    public override void Move() 
+    {
+        
+        if (road.Count > 0)
+        {
+            currentTarget = road[0];
+            if (Vector3.Distance(transform.position, currentTarget) < .1f)
+            {
+                road.RemoveAt(0);
+                currentTarget = road[0];
+            }
+            else
+            {
+                transform.position = Vector3.MoveTowards(transform.position, currentTarget, 10 * Time.deltaTime);
+            }
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, 10 * Time.deltaTime);
+        }
+
+        //currentState.Move();
+    }
+
+    public override void Fire()
+    {
+        //currentState.Fire();
+    }
 }
