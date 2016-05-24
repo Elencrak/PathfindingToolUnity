7bf400ed0e56080ce7f91c15d3b2097ba9bc7d7e
unknown <pi.martinez@J001-0135-05.etudiant.rubika-edu.com.local>
2016-05-24 15:50:27 +0200

    yes

---
+++ Assets/Jojo/State/Idle.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace JojoKiller {
+
+    public class Idle : IStateAgent{
+
+        public Idle(Member currentAgent)
+        {
+            member = currentAgent;
+        }
+        void Awake()
+        {
+        
+        }
+
+        public override IState execution()
+        {
+            
+            IState temp = base.execution();
+            if(temp == this)
+            {
+                Debug.Log("Idle");
+                member.idle();
+            }                        
+            return temp;           
+            /* if (needToChangeTarget || nextMove <= 0)
+             {
+                 targetPosition = stateTargets[0];
+                 foreach (Transform g in stateTargets)
+                 {
+                     if (g.GetInstanceID() != gameObject.GetInstanceID())
+                     {
+                         Vector3 relativePosition;
+                         Vector3 relativePositionTarget;
+                         relativePosition = g.position - transform.position;
+                         relativePositionTarget = targetPosition.position - transform.position;
+                         if (relativePositionTarget.magnitude > relativePosition.magnitude)
+                         {
+                             targetPosition = g;
+                         }
+                     }
+                 }
+                 needToChangeTarget = false;
+                 nextMove = move;
+
+                 currentNavMeshAgent.SetDestination(targetPosition.position);
+             }
+             else
+             {
+                 nextMove -= Time.deltaTime;
+             }*/
+        }
+    }
+}
